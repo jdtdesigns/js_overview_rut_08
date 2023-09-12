6e7ae5c5b96bd9ca0124ec6bc310d41ad88d9a98
JD Tadlock <jt7903@gmail.com>
2023-09-12 13:43:14 -0400

    web api basics complete

--- js/script.js
+++ js/script.js
@@ -1,29 +1,21 @@
-var fruits = ['orange', 'apple', 'kiwi'];
 
-var newFruits = fruits.map(function (fruit) {
-  return 'Fruit: ' + fruit;
-});
+var h1 = document.getElementById('main-header');
+var wrapperDiv = document.querySelector('#wrapper-div');
+var image = document.querySelector('#image');
+var h3 = document.querySelector('#nested-header');
 
-console.log(fruits);
-console.log(newFruits);
+// console.log(h1.innerText);
 
-// for (var i = 0; i < fruits.length; i++) {
-//   console.log(fruits[i]);
-// }
+// h1.innerText = '<p>Changed Text</p>';
+
+h1.style.color = 'red';
+h1.style.fontSize = '35px';
+
+wrapperDiv.innerHTML = '<p class="text-center">Some inner paragraph</p>';
+
+h1.classList.add('text-center');
+
+// h1.setAttribute('class', 'text-center');
 
 
-// function someFunc(num, str, callback) {
-//   callback('cb string', 5);
-// }
-
-// someFunc(10, 'some string', function (cbStr, cbNum) { console.log(cbNum) });
-
-
-// var arr = {
-//   items: ['orange', 'apple', 'kiwi'],
-//   forEach: function(cb) {
-//     for (var i = 0; i < arr.length; i++) {
-//       cb(this.items[i]);
-//     }
-//   }
-// }
+image.setAttribute('src', 'https://picsum.photos/200/300');
