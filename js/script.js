875b105549b6f3dc0aa2afc789763c49ec8bdad7
JD Tadlock <jt7903@gmail.com>
2023-09-12 11:36:49 -0400

    array method basics done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
+var fruits = ['orange', 'apple', 'kiwi'];
 
-// Determine the data
+var newFruits = fruits.map(function (fruit) {
+  return 'Fruit: ' + fruit;
+});
 
-// How do I store the data in the most organized way to utilize it for the app?
+console.log(fruits);
+console.log(newFruits);
 
-var questions = [
-  // questionText, choices, correctAnswer
-  {
-    questionText: 'What does JS stand for?',
-    choices: ['JustStand', 'JustSkying', 'JokeSyllabus', 'Javascript'],
-    correctAnswer: 'Javascript',
-    correctIndex: 3
-  },
-  {
-    questionText: 'What does the acronym D.O.M. stand for?',
-    choices: ['Do Only Math', 'Door Only Marketing', 'Document Object Model', 'Dragons Only Make-a-mess'],
-    correctAnswer: 'Javascript',
-    correctIndex: 2
-  }
-];
-
-console.log(questions[0]);
-console.log(questions[0].choices[2])
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
 
 
+// function someFunc(num, str, callback) {
+//   callback('cb string', 5);
+// }
 
-// var question1Text = 'What does JS stand for?';
-// var question1Choices = ['JustStand', 'JustSkying', 'JokeSyllabus', 'Javascript'];
-// var question1Answer = 'Javascript';
+// someFunc(10, 'some string', function (cbStr, cbNum) { console.log(cbNum) });
+
+
+// var arr = {
+//   items: ['orange', 'apple', 'kiwi'],
+//   forEach: function(cb) {
+//     for (var i = 0; i < arr.length; i++) {
+//       cb(this.items[i]);
+//     }
+//   }
+// }
