3241ac699467daa238b89687baa4e009c3692d3e
JD Tadlock <jt7903@gmail.com>
2023-09-13 10:04:58 -0400

    foreach overview done

--- js/script.js
+++ js/script.js
@@ -1,21 +1,7 @@
 
-var h1 = document.getElementById('main-header');
-var wrapperDiv = document.querySelector('#wrapper-div');
-var image = document.querySelector('#image');
-var h3 = document.querySelector('#nested-header');
 
-// console.log(h1.innerText);
+var names = ['Lorchesca', 'Brian', 'Michael', 'Hunter'];
 
-// h1.innerText = '<p>Changed Text</p>';
-
-h1.style.color = 'red';
-h1.style.fontSize = '35px';
-
-wrapperDiv.innerHTML = '<p class="text-center">Some inner paragraph</p>';
-
-h1.classList.add('text-center');
-
-// h1.setAttribute('class', 'text-center');
-
-
-image.setAttribute('src', 'https://picsum.photos/200/300');
+names.forEach(function (adsfasdfsadfsdfsadf) {
+  console.log(adsfasdfsadfsdfsadf);
+});
