0a504327ca5cc6131d07fd889692df0b438a42cc
JD Tadlock <jt7903@gmail.com>
2023-09-14 09:23:09 -0400

    data attributes and questions overview done

---
+++ js/questions.js
@@ -0,0 +1,10 @@
+var questions = [
+  {
+    text: 'What does JS stand for?',
+    choices: ['JustSittin', 'JustSailing', 'JustSkying', 'Javascript'],
+    correctAnswer: 'Javascript'
+  },
+  {
+
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,7 +1,117 @@
 
+var h1 = document.querySelector('#main-header');
+var choicesDiv = document.querySelector('.choices');
+var addBtn = document.querySelector('#add');
 
-var names = ['Lorchesca', 'Brian', 'Michael', 'Hunter'];
+var choices = ['DOM', 'BOB', 'DOP', 'DOT'];
+var correctAnswer = 'DOM';
+var score = 0;
 
-names.forEach(function (adsfasdfsadfsdfsadf) {
-  console.log(adsfasdfsadfsdfsadf);
+choices.forEach(function (choice) {
+  var btn = document.createElement('button'); // <button></button>
+
+  btn.innerText = choice;
+
+  if (choice === correctAnswer) {
+    btn.dataset.correct = true;
+  }
+
+  // btn.addEventListener('click', function () {
+  //   if (this.dataset.correct) {
+  //     alert('You are correct!');
+  //   } else {
+  //     alert('You are wrong!');
+  //   }
+  // });
+
+  choicesDiv.append(btn);
 });
+
+// document.body.addEventListener('', function () {
+//   console.log('body clicked');
+// })
+
+function checkAnswer(eventObj) {
+  var el = eventObj.target;
+
+  if (el.tagName === 'BUTTON') {
+    if (el.dataset.correct) {
+      score += 10;
+      console.log('you are correct!');
+    } else {
+      console.log('Incorrect!');
+    }
+  }
+}
+
+choicesDiv.addEventListener('click', checkAnswer);
+
+
+function addChoice() {
+  choicesDiv.insertAdjacentHTML('beforeend', '<button>Another</button>');
+}
+
+addBtn.addEventListener('click', addChoice);
+
+
+console.log(questions);
+
+
+
+
+
+
+// var choices = ['DOM', 'BOB', 'DOP', 'DOT'];
+// var html = '';
+
+// choices.forEach(function (choice) {
+//   var btn = '<button>' + choice + '</button>';
+
+//   html += btn;
+// });
+
+// console.log(html);
+
+// choicesDiv.innerHTML = html;
+
+
+
+
+
+
+
+
+
+
+// var firstBtn = btns[0];
+// var secondBtn = btns[1];
+
+// if (firstBtn.dataset.correct === 'true') {
+//   console.log('You are correct!');
+// }
+
+// if (secondBtn.dataset.correct === 'true') {
+//   console.log('You are correct 2nd button');
+// }
+
+
+
+
+
+// h1.style.color = 'red';
+
+
+// var h1 = {
+//   style: {
+//     color: ''
+//   }
+// };
+
+
+// h1.style.color = 'orange';
+
+// var jd = {
+//   hairColor: 'white',
+//   eyeColor: 'grey',
+//   height: '6\'2"'
+// };
