5658581ca1b49ff6b4c3676278ac97484344bd25
JD Tadlock <jt7903@gmail.com>
2023-09-14 10:39:32 -0400

    event listener overview done

--- js/script.js
+++ js/script.js
@@ -1,117 +1,29 @@
 
-var h1 = document.querySelector('#main-header');
-var choicesDiv = document.querySelector('.choices');
-var addBtn = document.querySelector('#add');
-
-var choices = ['DOM', 'BOB', 'DOP', 'DOT'];
-var correctAnswer = 'DOM';
-var score = 0;
-
-choices.forEach(function (choice) {
-  var btn = document.createElement('button'); // <button></button>
-
-  btn.innerText = choice;
-
-  if (choice === correctAnswer) {
-    btn.dataset.correct = true;
-  }
-
-  // btn.addEventListener('click', function () {
-  //   if (this.dataset.correct) {
-  //     alert('You are correct!');
-  //   } else {
-  //     alert('You are wrong!');
-  //   }
-  // });
-
-  choicesDiv.append(btn);
-});
-
-// document.body.addEventListener('', function () {
-//   console.log('body clicked');
-// })
-
-function checkAnswer(eventObj) {
-  var el = eventObj.target;
-
-  if (el.tagName === 'BUTTON') {
-    if (el.dataset.correct) {
-      score += 10;
-      console.log('you are correct!');
-    } else {
-      console.log('Incorrect!');
-    }
-  }
-}
-
-choicesDiv.addEventListener('click', checkAnswer);
-
-
-function addChoice() {
-  choicesDiv.insertAdjacentHTML('beforeend', '<button>Another</button>');
-}
-
-addBtn.addEventListener('click', addChoice);
-
-
-console.log(questions);
-
-
-
-
-
-
-// var choices = ['DOM', 'BOB', 'DOP', 'DOT'];
-// var html = '';
-
-// choices.forEach(function (choice) {
-//   var btn = '<button>' + choice + '</button>';
-
-//   html += btn;
+// document.body.addEventListener('click', function (eventObj) {
+//   console.log(eventObj.target);
 // });
 
-// console.log(html);
+var toggle = document.querySelector('.toggle-bar');
 
-// choicesDiv.innerHTML = html;
+toggle.addEventListener('click', function () {
+  var toggleSwitch = document.querySelector('.toggle');
 
-
-
-
-
-
+  toggleSwitch.classList.toggle('switch');
+});
 
 
 
 
-// var firstBtn = btns[0];
-// var secondBtn = btns[1];
+// var btn = document.querySelector('#add');
 
-// if (firstBtn.dataset.correct === 'true') {
-//   console.log('You are correct!');
-// }
+// btn.addEventListener('click', function () {
+//   console.log('clicked')
+// });
 
-// if (secondBtn.dataset.correct === 'true') {
-//   console.log('You are correct 2nd button');
-// }
+// document.addEventListener('keyup', function (eventObj) {
+//   var key = eventObj.keyCode || eventObj.key;
 
-
-
-
-
-// h1.style.color = 'red';
-
-
-// var h1 = {
-//   style: {
-//     color: ''
+//   if (key === 13 || key === 'enter') {
+//     console.log('you pressed the enter key');
 //   }
-// };
-
-
-// h1.style.color = 'orange';
-
-// var jd = {
-//   hairColor: 'white',
-//   eyeColor: 'grey',
-//   height: '6\'2"'
-// };
+// });
